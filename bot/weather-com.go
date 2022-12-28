960e78107e8364f3ba2800e13f1be3f8b00d8016
Luis Dourado <lfaddourado@gmail.com>
2022-12-28 00:52:56 -0300

    feat: add Weather Data struct

---
+++ bot/weather-com.go
@@ -0,0 +1,17 @@
+package bot
+
+const URL string = "https://api.openweathermap.org/data/2.5/weather?units=metric&"
+
+type WeatherData struct {
+	Weather []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+	Main struct {
+		Temp     float64 `json:"temp"`
+		Humidity float64 `json:"humidity"`
+	} `json:"main"`
+	Wind struct {
+		Speed float64 `json:"speed"`
+	} `json:"wind"`
+	Name string `json:"name"`
+}
