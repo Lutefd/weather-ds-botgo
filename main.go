da0a5572cc3bb3075894f9ca7ef072db23e7d79c
Luis Dourado <lfaddourado@gmail.com>
2022-12-28 00:22:04 -0300

    feat: import bot into main

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import "log"
+import (
+	"github.com/Lutefd/weather-ds-botgo/bot"
+	"log"
+	"os"
+)
 
 func main() {
 
-	botToken, ok := os.LookupEbv("BOT_TOKEN")
+	botToken, ok := os.LookupEnv("BOT_TOKEN")
 	if !ok {
 		log.Fatal("BOT_TOKEN não encontrado")
 	}
-	openWeatherToken, ok := os.LookupEbv("OPEN_WEATHER_TOKEN")
+	openWeatherToken, ok := os.LookupEnv("OPEN_WEATHER_TOKEN")
 	if !ok {
 		log.Fatal("OPEN_WEATHER_TOKEN não encontrado")
 	}
